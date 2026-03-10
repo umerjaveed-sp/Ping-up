68425039d19781b9703e940bc162b89806b0d6fe
umer javeed <umersp6@gmail.com>
2026-03-10 10:33:43 +0500

    inngest error sort

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -9,12 +9,13 @@ const syncUserCreation = inngest.createFunction(
   { id: "sync-user-from-clerk" },
   { event: "clerk/user.created" },
   async ({ event }) => {
-    const { id, first_name, last_name, email_address, image_url } = event.data;
-    let usename = email_address[0].email_address.split("@")[0];
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    let username = email_addresses[0].email_address.split("@")[0];
 
     // Check avalibilty of user
 
-    const user = await User.findOne({ usernme });
+    const user = await User.findOne({ username });
 
     if (user) {
       username = username + Math.floor(Math.random() * 1000);
