f1888edabaf411c5af10be942501eaf833cc36d5
umer javeed <umersp6@gmail.com>
2026-03-10 10:46:08 +0500

    inngest error sort3

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -22,7 +22,7 @@ const syncUserCreation = inngest.createFunction(
     }
     const userData = {
       _id: id,
-      email: email_address[0].email_address,
+      email: email_addresses[0].email_address,
       full_name: first_name + "" + last_name,
       profile_picture: image_url,
       username,
